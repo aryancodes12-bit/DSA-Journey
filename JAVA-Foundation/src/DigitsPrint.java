1a689ec6cdad0f3f461bbfc443e6726f77989f3e
AryanCodes404-found <aj9002468@gmail.com>
2025-07-17 00:42:37 +0530

    fixed bug

--- JAVA-Foundation/src/DigitsPrint.java
+++ JAVA-Foundation/src/DigitsPrint.java
@@ -5,12 +5,13 @@ public class DigitsPrint {
         Scanner sc=new Scanner(System.in);
         int n= sc.nextInt();
         int nod=0;
-        while(n !=0){
-            n=n/10;
+        int temp=n;
+        while(temp !=0){
+            temp=temp/10;
             nod++;
         }
         int div=(int)Math.pow(10,nod-1);
-        while(div !=0){
+        while(n !=0){
             int q=n/div;
             System.out.println(q);
             n=n%div;
