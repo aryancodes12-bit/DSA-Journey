687951b36f602dc46cd99c31f9e78f550876439a
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-07-21 23:00:32 +0530

    Auto update: 2026-07-21 23:00:32

--- A2Z DSA JAVA/src/Leetcode/LC1929.java
+++ A2Z DSA JAVA/src/Leetcode/LC1929.java
@@ -1,9 +1,23 @@
 package Leetcode;
 
+import java.util.Arrays;
+
 public class LC1929 {
     public int[] getConcatenation(int[] nums) {
+        int n =nums.length;
+ int[] ans = Arrays.copyOf(nums,nums.length*2);
 
-        return nums;
+ for (int i=0;i<n;i++){
+     ans[i]=nums[i];
+     ans[i+n]=nums[i];
+ }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        LC1929 l=new LC1929();
+       int [] nums={1,2,3};
+        System.out.println(Arrays.toString(l.getConcatenation(nums)));
     }
     }
 
