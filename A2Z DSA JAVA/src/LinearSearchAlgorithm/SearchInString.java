0bdcdb6473d29beb559c39338c67680c15b884c2
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-07-22 19:57:41 +0530

    Auto update: 2026-07-22 19:57:41

--- A2Z DSA JAVA/src/LinearSearchAlgorithm/SearchInString.java
+++ A2Z DSA JAVA/src/LinearSearchAlgorithm/SearchInString.java
@@ -2,27 +2,38 @@ package LinearSearchAlgorithm;
 
 public class SearchInString {
     public static void main(String[] args) {
-        String[] str = {"aryan", "anagha", "riyaRaj"};
-        String target = "anagha";
+        String str = "aryan";
+        char target = 'a';
 
-        // Call the search method
-        search(str, target);
+        System.out.println(search(str, target));
     }
 
-    static void search(String[] str, String target) {
-        if (str.length == 0) {
-            System.out.println("Array is empty");
-            return; // Exit early if empty
-        }
+ //   static void search(String[] str, String target) {
+//        if (str.length == 0) {
+//            System.out.println("Array is empty");
+//            return; // Exit early if empty
+//        }
+//
+//        for (int i = 0; i < str.length; i++) {
+//            // Directly compare strings using .equals()
+//            if (str[i].equals(target)) {
+//                System.out.println("Found at index " + i);
+//                return; // Stop searching once found
+//            }
+//        }
+//
+//        System.out.println("Element not found in the array");
+//    }
 
-        for (int i = 0; i < str.length; i++) {
-            // Directly compare strings using .equals()
-            if (str[i].equals(target)) {
-                System.out.println("Found at index " + i);
-                return; // Stop searching once found
+    static boolean search(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == target) {
+                return true;
             }
         }
-
-        System.out.println("Element not found in the array");
+        return false;
     }
 }
