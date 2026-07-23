d9fe7f16875ae1b396e71024280965133f713194
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-07-23 17:17:16 +0530

    Auto update: 2026-07-23 17:17:16

---
+++ A2Z DSA JAVA/src/LinearSearchAlgorithm/Leetcode/LC754.java
@@ -0,0 +1,29 @@
+package LinearSearchAlgorithm.Leetcode;
+
+import java.util.Arrays;
+
+public class LC754 {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+
+        // Step 1: Calculate the total sum of all elements
+        for (int num : nums) {
+            totalSum += num;
+        }
+            int leftSum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                int right = totalSum - leftSum - nums[i];
+                if (leftSum == right) {
+                    return i;
+                }
+                leftSum += nums[i];
+            }
+
+         return -1;
+    }
+    public static void main(String[] args) {
+        LC754 l=new LC754();
+        int []  nums={1,7,3,6,5,6};
+     int sol=l.pivotIndex(nums);
+        System.out.println(sol);}
+}
