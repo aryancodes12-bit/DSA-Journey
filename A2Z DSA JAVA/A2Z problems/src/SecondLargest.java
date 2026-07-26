427bda8e0b0c5a8fe7e084806f70649036d450c6
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-07-26 16:42:41 +0530

    Auto update: 2026-07-26 16:42:41

---
+++ A2Z DSA JAVA/A2Z problems/src/SecondLargest.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class SecondLargest {
+    long first = 0;
+    long second = 0;
+
+    public int secondLargestElement(int[] nums) {
+        if (nums.length < 2) return -1;
+        Arrays.sort(nums);
+        int largest = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] != largest) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        SecondLargest s=new SecondLargest();
+        int [] nums= {7, 7, 2, 2, 10, 10, 10};
+        System.out.println(s.secondLargestElement(nums));
+    }
+}
