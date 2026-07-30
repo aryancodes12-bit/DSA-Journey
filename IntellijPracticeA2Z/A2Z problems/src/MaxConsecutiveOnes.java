215a9b2385a712383d3f69d0414294adbd1b9ff7
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-07-30 20:20:37 +0530

    Auto update: 2026-07-30 20:20:37

---
+++ IntellijPracticeA2Z/A2Z problems/src/MaxConsecutiveOnes.java
@@ -0,0 +1,18 @@
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+int maxCount=0; int count=0;
+for(int i=0;i< nums.length;i++){
+    if(nums[i]==1){
+        count++;
+
+    if(count>maxCount){
+        maxCount=count;
+    }}
+    else {
+        count=0;
+    }
+
+}
+        return maxCount;
+    }
+}
