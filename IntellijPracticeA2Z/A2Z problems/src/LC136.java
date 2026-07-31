1772806fae23afa15bb4b5e7323ee337f84334c5
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-08-01 00:12:46 +0530

    Update - 2026-08-01 00:12:46

--- IntellijPracticeA2Z/A2Z problems/src/LC136.java
+++ IntellijPracticeA2Z/A2Z problems/src/LC136.java
@@ -3,10 +3,27 @@ import java.util.ArrayList;
 
 public class LC136 {
     public int singleNumber(int[] nums) {
-        int result=0;
-        for (int i =0;i<nums.length;i++){
-           result^=nums[i];
-        }
-        return result;
+        // optimal
+//        int result=0;
+//        for (int i =0;i<nums.length;i++){
+//           result^=nums[i];
+//        }
+//        return result;
+
+        // brute force
+        int max=nums[0];
+for (int i=0;i<nums.length;i++){
+    max=Math.max(max,nums[i]);
+}
+int []hash =new int[max+1];
+for (int i=0;i<nums.length;i++){
+    hash[nums[i]]++;
+}
+for (int i=0;i< nums.length;i++){
+    if(hash[nums[i]]==1){
+        return i;
+    }
+}
+return -1;
     }
 }
