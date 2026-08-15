0c8f7e2e2a8b2d65e13d1998cc962f56c7b10238
ARYAN JAISWAL <aryanjaiswal3080@gmail.com>
2026-08-15 14:13:39 +0530

    Add LC121 - Best Time to Buy and Sell Stock

---
+++ organized/Array/LC121/LC121.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n =prices.length;
+        int minPrice=prices[0];
+ int maxProfit=0;
+ for (int i=1;i< n ;i++){
+     if(prices[i]<minPrice){
+         minPrice=prices[i];
+     }
+     int profit=prices[i]-minPrice;
+     if(profit>maxProfit){
+         maxProfit=profit;
+     }
+ }
+ return maxProfit;
+    } 
+    
+}
