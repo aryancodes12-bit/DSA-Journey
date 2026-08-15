1c084016652f40a29c94c2e99031d8d9bca40a9b
ARYAN JAISWAL <aryanjaiswal3080@gmail.com>
2026-08-15 22:06:59 +0530

    Add LC31 - Next Permutation

---
+++ organized/Array/LC31/LC31.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+int ind=-1;
+        for (int i = n-2; i >=0 ; i--) {
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
+            }
+        }
+        if(ind==-1){
+            rev(nums,0,n-1);
+            return;
+        }
+        for(int i=n-1;i>=ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums,i,ind);
+                break;
+            }
+        }
+        rev(nums,ind+1,n-1);
+    }
+    void rev(int [] nums, int start, int end){
+        while(start<end){
+            swap(nums,start,end);
+            start++;
+            end--;
+        }
+    }
+    void swap(int [] nums,int i, int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    
+    }
+}
