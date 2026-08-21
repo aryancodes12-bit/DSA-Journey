4ba40a93e6d8bf4dadae07edd21f2b1ccaaf9080
ARYAN JAISWAL <aryanjaiswal3080@gmail.com>
2026-08-22 00:52:45 +0530

    Add LC119 - Pascal's Triangle II

--- organized/Array/PascalSTriangleII/PascalSTriangleII.java
+++ organized/Array/PascalSTriangleII/PascalSTriangleII.java
@@ -1,17 +1,12 @@
 import java.util.*;
 class Solution {
-    public int function(int n,int r){
-        long res=1;
-        for(int i=0;i<r;i++){
-            res=res*(n-i);
-        res=res/(i+1);
-        }
-        return (int) res;
-    }
     public List<Integer> getRow(int rowIndex) {
         List<Integer> l=new ArrayList<>();
-for(int c=0;c<=rowIndex;c++){
-l.add(function(rowIndex,c));
+        long res=1;
+        l.add((int)res);
+for(int c=1;c<=rowIndex;c++){
+res=res*(rowIndex-c+1)/c;
+l.add((int)res);
 }
  return l;
     }
