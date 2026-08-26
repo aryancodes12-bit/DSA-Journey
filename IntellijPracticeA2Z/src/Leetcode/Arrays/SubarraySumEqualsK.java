8d477998686ee2ae7796ae2bc59fe699f8a7574f
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-08-26 21:35:04 +0530

    Update - 2026-08-26 21:35:04

--- IntellijPracticeA2Z/src/Leetcode/Arrays/SubarraySumEqualsK.java
+++ IntellijPracticeA2Z/src/Leetcode/Arrays/SubarraySumEqualsK.java
@@ -31,5 +31,20 @@ public class SubarraySumEqualsK {
 //        return count;
 //    }
 //}
+        /*
+          int n =nums.length; int s=0; int cnt=0;
+       Map<Integer,Integer> h=new HashMap<>();
+        h.put(0,1);
+       for (int i=0;i<n;i++){
+        s+=nums[i];
+
+ int prefix=s-k;
+ if(h.containsKey(prefix)){
+    cnt+=h.get(prefix);
+ }
+h.put(s,h.getOrDefault(s,0)+1);
+       }
+       return cnt;
+         */
         
 }
