95fe64042e28bcbbcd0ee7d61073361db9ebe187
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-09-14 23:03:10 +0530

    Update - 2026-09-14 23:03:10

--- IntellijPracticeA2Z/BinarySearch/src/RecursiveApproach.java
+++ IntellijPracticeA2Z/BinarySearch/src/RecursiveApproach.java
@@ -1,2 +1,19 @@
 public class RecursiveApproach {
+    public int bs(int[] nums,int low,int high, int target) {
+        if (low > high) {
+            return -1;
+        }
+        int mid=(low+high)/2;
+        if(nums[mid]==target){
+            return mid;
+        } else if (target>nums[mid]) {
+            return bs(nums,mid+1,high,target);
+        }
+        else {
+            return bs(nums,low,mid-1,target);
+        }
+    }
+    public int search(int [] nums, int target){
+        return bs(nums,0,nums.length-1,target);
+    }
 }
