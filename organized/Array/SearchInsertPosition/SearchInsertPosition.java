ef592cf0831bd0636bf58fc64389cb3e7e4f7885
ARYAN JAISWAL <aryanjaiswal3080@gmail.com>
2026-09-15 21:39:37 +0530

    Add LC35 - Search Insert Position

---
+++ organized/Array/SearchInsertPosition/SearchInsertPosition.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+                 int n =nums.length;
+  int low=0;
+  int high=n-1;
+  int ans=n;
+  int temp=ans;
+while(low<=high){
+int mid=low+(high-low)/2;
+if(nums[mid]>=target){
+    ans=mid;
+    high=mid-1;
+}
+else{
+    low=mid+1;
+}
+if(temp==n){
+    temp=low+1;
+}
+}
+return ans;
+    }
+}
