8567bb30161e11b5dc1cf4dfe674115bb9af9ca3
Aryan Jaiswal <aryanjaiswal3080@gmail.com>
2026-09-22 00:43:55 +0530

    Update - 2026-09-22 00:43:55

---
+++ IntellijPracticeA2Z/BinarySearch/On Answers/src/findKthPositive.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class findKthPositive {
+    //    public int findKthPositive(int[] arr, int k) {
+//
+//        int n=arr.length;
+//        int flag=0;
+//        int max=0;
+//        for(int i:arr){
+//            max=Math.max(max,i);
+//        }
+//        for(int i=1;i<=max;i++){
+//            boolean found=false;
+//            for(int j=0;j<n;j++) {
+//                if (arr[j] == i) {
+//                    found = true;
+//                    break;
+//                }
+//            }
+//            if(!found){
+//                flag++;
+//
+//                if(flag==k){
+//                    return i;
+//                }
+//            }
+//    return max + (k - flag);
+//     }
+//    }
+//}
+    public int findKthPositive(int[] arr, int k) {
+
+    }
+}
