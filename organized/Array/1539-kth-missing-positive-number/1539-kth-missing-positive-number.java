7f596f74c6a0e13963a0fd2de0cc3c85d534ccec
ARYAN JAISWAL <aryanjaiswal3080@gmail.com>
2026-09-22 23:52:33 +0530

    Add LC1539 - Kth Missing Positive Number

--- organized/Array/1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ organized/Array/1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,22 +1,16 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
 
-        int low = 0;
-        int high = arr.length - 1;
-
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            int missing = arr[mid] - (mid + 1);
-
-            if (missing < k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+               int n=arr.length;
+     for(int i=0;i<n;i++){
+        if(arr[i]<=k){
+            k++;
         }
-
-        return low + k;
+        else {
+            break;
+        }
+     }
+     return k;
     }
 }
+
